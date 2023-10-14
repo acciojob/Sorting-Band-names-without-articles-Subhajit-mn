f43212f629ec4a93ca4fe0ba55d3a14c42a49067
Subhajit Mandal <108950928+Subhajit-mn@users.noreply.github.com>
2023-10-14 10:54:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your code here
+function strip(bandName) {
+	return bandName.replace(/^(a | an| the)/i, '').trim();
+}
+const sortedBands = band.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 
+document.querySelector('#band').innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('');
+console.log(sortedBands);
